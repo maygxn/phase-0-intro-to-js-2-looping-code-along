ce282e95f9ba55b140d71aa5b019d2879ae0930c
Meagan <MeaganTruong1@yahoo.com>
2023-10-10 23:10:49 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]; 
+    function writeCards(names, event) {
+        const messages = []
+        for (let i = 0; i < names.length; i++) {
+            messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+            console.log(messages[i])
+        }
+        return messages;
+    }
+writeCards(names);
+
+function countDown() {
+    let countDown = 0
+    while (countDown < 11) {
+        console.log(countDown++);
+    }
+}
